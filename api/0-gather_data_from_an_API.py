0c56967692ad2ca96f107c2a9cc4cab63704a6d4
isamuella <s.ineza@alustudent.com>
2025-02-13 15:17:43 +0000

    ex0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,16 +2,16 @@
 """
 Request from API; return todo list of an employee using his ID
 """
-import requests
+import requests # type: ignore
 import sys
 
 
-def get_todo_progress(employee_id):
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = f"{base_url}/users/{employee_id}"
-    todos_url = f"{base_url}/todos?userId={employee_id}"
+def get_todo_lists(employee_id):
+    main_url = "https://jsonplaceholder.typicode.com"
+    user_url = f"{main_url}/users/{employee_id}"
+    todo_url = f"{main_url}/todos?userId={employee_id}"
 
-    # Fetch user data
+    """ Fetch user data """
     user_response = requests.get(user_url)
     if user_response.status_code != 200:
         print("Error: User not found")
@@ -20,17 +20,17 @@ def get_todo_progress(employee_id):
     user_data = user_response.json()
     employee_name = user_data.get("name")
 
-    # Fetch TODO list data
-    todos_response = requests.get(todos_url)
-    if todos_response.status_code != 200:
+    """ Fetch the todo list data """
+    todo_response = requests.get(todo_url)
+    if todo_response.status_code != 200:
         print("Error: Could not fetch tasks")
         return
 
-    todos_data = todos_response.json()
-    total_tasks = len(todos_data)
-    done_tasks = [task for task in todos_data if task.get("completed")]
+    todo_data = todo_response.json()
+    total_tasks = len(todo_data)
+    done_tasks = [task for task in todo_data if task.get("completed")]
 
-    print(f"Employee {employee_name} is
+    print(f"Employee {employee_name} is 
           done with tasks({len(done_tasks)}/{total_tasks}):")
     for task in done_tasks:
         print(f"\t {task.get('title')}")
@@ -41,6 +41,6 @@ if __name__ == "__main__":
     else:
         try:
             employee_id = int(sys.argv[1])
-            get_todo_progress(employee_id)
+            get_todo_lists(employee_id)
         except ValueError:
             print("Error: Employee ID must be an integer")
